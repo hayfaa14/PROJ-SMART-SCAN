c67b71533931b2bff0cae7073e42f9a0f8517632
popozoulou <charlie.joulou@gmail.com>
2021-03-08 10:40:29 +0100

    changement

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import 'materialize-css';
 
-var elems = document.querySelectorAll('.fixed-action-btn');
-var instances = M.FloatingActionButton.init(elems, options);
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, options);
+});
