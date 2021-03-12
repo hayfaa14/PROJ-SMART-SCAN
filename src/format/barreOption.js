ca091289b28b2eee6492ac6bd3282edcb9de709b
popozoulou <charlie.joulou@gmail.com>
2021-03-12 12:21:00 +0100

    fixes

--- src/format/barreOption.js
+++ src/format/barreOption.js
@@ -2,7 +2,7 @@ export const barreOptionJS = (selector) => {
     const element = document.querySelector(selector);
     element.innerHTML = `
     <div class="fixed-action-btn direction-bottom click-to-toggle topleftcorner">
-        <a class="btn-floating btn-large waves-effect waves-light greenproject "><i class="material-icons">add</i></a>
+        <a class="btn-floating btn-large waves-effect waves-light greenproject"><i class="material-icons">add</i></a>
             <ul>
                 <li>
                     <a href="rectangle_cut.php" class="btn-floating greyproject" type="button"><i class="material-icons">crop</i></a>
@@ -15,11 +15,10 @@ export const barreOptionJS = (selector) => {
                 </li>
             </ul>
     </div>
-    <a href="formatText.html" class="btn-floating btn-large waves-effect waves-light greenproject savebutton" type="button"><i class="material-icons">save</i></a>
+    <a href="formatText.js" class="btn-floating btn-large waves-effect waves-light greenproject savebutton" type="button"><i class="material-icons">save</i></a>
     </div>
     <br>
     `
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, { direction: 'bottom', hoverEnabled: false });
 };
-
-var elems = document.querySelectorAll('.fixed-action-btn');
-var instances = M.FloatingActionButton.init(elems, { direction: 'bottom', hoverEnabled: false });
