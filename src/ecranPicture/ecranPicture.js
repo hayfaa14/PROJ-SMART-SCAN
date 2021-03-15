ef3f12d73b021f0caf654090f382f978d2758a44
harleyqueen7577 <celine.lenclud@gmail.com>
2021-03-15 09:38:46 +0100

    mise en forme pour ressembler à un frameworks

--- src/ecranPicture/ecranPicture.js
+++ src/ecranPicture/ecranPicture.js
@@ -9,5 +9,4 @@ export const displayEcranPicture= (element) =>{
         displaySave(document.body);
         return false;
     }
-    
 }
--- src/home/home.js
+++ src/home/home.js
@@ -3,11 +3,10 @@ import template from './home.html';
 
 export const displayHome= (element) =>{
     element.innerHTML = template;
-    
+
   const linkStart = document.querySelector("#Picture");
   linkStart.onclick = ()=>{ 
       displayEcranPicture(document.body);
       return false;
     }
- 
 }
